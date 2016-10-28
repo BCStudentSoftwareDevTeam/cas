9167ceaf922a05ba20a710ad3cadf3e90494d197
Cody Myers <codymyers63@gmail.com>
2016-10-28 16:43:08 +0000

    Allowing multiple user to access flask-admin

--- app/databaseAdmin.py
+++ app/databaseAdmin.py
@@ -2,9 +2,13 @@ from allImports import *
 from flask_admin.contrib.peewee import ModelView
 
 class AuthenticatedUser(ModelView):
-    
     def is_accessible(self):
-        return authUser(request.environ) == cfg['databaseAdmin']['user']
+        access = False
+        for x in cfg['databaseAdmin']['user']:
+            if authUser(request.environ) == x:
+                access = True
+        return access
+        
     
 admin.add_view(AuthenticatedUser(Division))
 admin.add_view(AuthenticatedUser(BannerSchedule))
@@ -13,13 +17,11 @@ admin.add_view(AuthenticatedUser(Rooms))
 admin.add_view(AuthenticatedUser(Program))
 admin.add_view(AuthenticatedUser(Subject))
 admin.add_view(AuthenticatedUser(User))
-
 admin.add_view(AuthenticatedUser(BannerCourses))
 admin.add_view(AuthenticatedUser(Course))
 admin.add_view(AuthenticatedUser(ProgramChair))
 admin.add_view(AuthenticatedUser(DivisionChair))
 admin.add_view(AuthenticatedUser(InstructorCourse))
 admin.add_view(AuthenticatedUser(Deadline))
-
 admin.add_view(AuthenticatedUser(CourseChange))
 admin.add_view(AuthenticatedUser(InstructorCourseChange))
