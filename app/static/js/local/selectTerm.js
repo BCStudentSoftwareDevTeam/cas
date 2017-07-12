d4a049a2922ec0e1ff948eb4353d9e023e15fce6
Cody Myers <codymyers63@gmail.com>
2017-07-12 14:13:41 +0000

    Got the functionality for the redirect sort of working

--- app/static/js/local/selectTerm.js
+++ app/static/js/local/selectTerm.js
@@ -3,11 +3,18 @@ $(window).on('load',function(){
     $('#selectTermModal').modal('show');
 });
 
-var termCode = document.getElementById("termInfo").value
+
+//Test last visited when empyt
+//fix redirect for else
+// remove the random print on the html that says CSC
+
 function redirectCourses(prefix){
+    var termCode = document.getElementById("termInfo").value
     console.log("The function is being used.")
-    if (lastVisited !== null){
-        window.location("/courses/termCode/" + prefix)
+    if (prefix !== null){
+        var href = "/courses/termCode/" + prefix
+        console.log(href)
+        window.location.replace("/courses/"+ termCode +"/"+ prefix)
     }
     else{
         window.location("/courses/termCode/MAT")
