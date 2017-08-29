9e0528641a802ef798444962bf0eb1f650c3ee38
Kenny Nelson <nelsonk@berea.edu>
2017-08-29 13:04:09 +0000

    Work done so far on the course table

--- app/courseTable.py
+++ app/courseTable.py
@@ -2,8 +2,8 @@ from allImports import *
 from app.logic.getAuthUser import AuthorizedUser
 from app.logic.databaseInterface import *
 
-@app.route("/courses/<pid_or_prefix>/", methods=["GET", "POST"])
-def retrieveCourses(pid_or_prefix):
+@app.route("/courses/<prefix>/", methods=["GET", "POST"])
+def retrieveCourses(prefix):
     # Retrieve 5 most recents years in the database to display courses from.
     terms = Term.select().distinct().order_by(Term.termCode.asc()).limit(10)
 
@@ -14,17 +14,9 @@ def retrieveCourses(pid_or_prefix):
     lowest_term = int(term_list.pop())
     
     try:
-        if pid_or_prefix.isnumeric():
-            # print("In the if")
-            # Retrieve all the courses that will be offered from each program
-            allCourses = BannerCourses.select().join(Subject).where(Subject.pid== pid_or_prefix)
-            # print("Out of the if")
-        else:
-            # print ("In the else")
-            allCourses = BannerCourses.select().join(Subject).where(Subject.prefix== pid_or_prefix)
+        allCourses = BannerCourses.select().join(Subject).where(Subject.prefix== prefix)
         
         courses_offered = [];
-        test_list =[]
         reFID_to_term = dict()
         for bannerRef in allCourses:
             print 'This is bannerRef{}'.format(bannerRef.reFID)
@@ -34,14 +26,13 @@ def retrieveCourses(pid_or_prefix):
                 terms_offered.append(course.term.termCode)
             reFID_to_term[bannerRef.reFID] = terms_offered
         print reFID_to_term            
-        return render_template("courseTable.html", terms = terms, allCourses = allCourses, cfg=cfg)
+        return render_template("courseTable.html", terms = terms, allCourses = allCourses, reFID_to_term = reFID_to_term, cfg=cfg)
     
     except Exception as e:
         print ("Here is the error:")
         print(e)
         return redirect('/')
-    # Check when each specific course will be offered.
-    
+
     
     
     
--- app/static/js/local/selectProgram.js
+++ app/static/js/local/selectProgram.js
@@ -6,6 +6,6 @@ $(window).on('load',function(){
 
 function redirectCourses(){
     console.log("I am not sure what is happening!")
-    var pid_or_prefix= document.getElementById("programInfo").value
-    window.location.replace("/courses/"+pid_or_prefix+"/")
+    var prefix= document.getElementById("programInfo").value
+    window.location.replace("/courses/"+prefix+"/")
 }
