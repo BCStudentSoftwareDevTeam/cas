4dab5abed674a6e1b0ee8adc537846d7d596616a
Kenny Nelson <nelsonk@berea.edu>
2017-09-26 16:07:57 +0000

    Created the course sequencing table, and added the x button that was missing from the select term modal

--- app/courseTable.py
+++ app/courseTable.py
@@ -4,7 +4,7 @@ from app.logic.databaseInterface import *
 
 @app.route("/courses/<prefix>/", methods=["GET", "POST"])
 def retrieveCourses(prefix):
-    # Retrieve 5 most recents years in the database to display courses from.
+    # Select 5 most recents years in the database to display courses from.
     terms = Term.select().distinct().order_by(Term.termCode.asc()).limit(10)
 
     term_list = []
@@ -14,19 +14,26 @@ def retrieveCourses(prefix):
     lowest_term = int(term_list.pop())
     
     try:
+        # allCourses is the list of all the classes for each program 
         allCourses = BannerCourses.select().join(Subject).where(Subject.prefix== prefix)
         
-        courses_offered = [];
-        reFID_to_term = dict()
+        subject = Subject.get(Subject.prefix==prefix)
+        
+        courses_offered = []
+        
+        # courseName_to_term is a dictionary that maps the course name to the semesters when it was offered
+        courseName_to_term = dict()
+        
         for bannerRef in allCourses:
-            print 'This is bannerRef{}'.format(bannerRef.reFID)
+            # ref_course is the list of all the classes that were offered for all the terms that were selected
             ref_course = Course.select().where(Course.bannerRef == bannerRef.reFID, Course.term >= lowest_term ).distinct()
             terms_offered = []
+            
             for course in ref_course:
                 terms_offered.append(course.term.termCode)
-            reFID_to_term[bannerRef.reFID] = terms_offered
-        print reFID_to_term            
-        return render_template("courseTable.html", terms = terms, allCourses = allCourses, reFID_to_term = reFID_to_term, cfg=cfg)
+            courseName_to_term[str(bannerRef.subject)+" "+ str(bannerRef.number)] = terms_offered
+            
+        return render_template("courseTable.html", terms = terms, allCourses = allCourses, courseName_to_term = courseName_to_term, subject = subject, cfg=cfg)
     
     except Exception as e:
         print ("Here is the error:")
