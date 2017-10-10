bbd41e21617437a3bf48bfe15b8a10854889f280
Cody Myers <codymyers63@gmail.com>
2017-10-10 16:00:53 +0000

    Issue #187: Fixed the controller that makes a course deactivated/activated in the database

--- app/editActiveCourses.py
+++ app/editActiveCourses.py
@@ -1,5 +1,6 @@
 from allImports import *
 from app.logic.getAuthUser import AuthorizedUser
+from app.logic.redirectBack import redirect_url
 
 @app.route("/edit/active/courses", methods=['GET'])
 def editAcitveCourses():
@@ -13,5 +14,33 @@ def editAcitveCourses():
                                 activeCourses   = activeCourses,
                                 deactiveCourses = deactiveCourses
                                )
+    else:
+        abort(403)
+        
+@app.route("/deactivate/course", methods=['POST'])
+def deactivateCourse():
+    authorizedUser = AuthorizedUser()
+    if authorizedUser.isAdmin():
+        data = request.form
+        Course = BannerCourses.get(BannerCourses.reFID == data['deactivate'])
+        Course.is_active = False
+        message = Course.subject.prefix + ' ' + Course.number + ' ' + Course.ctitle + ' has been deactivated'
+        Course.save()
+        flash(message)
+        return redirect(redirect_url())
+    else:
+        abort(403)
+        
+@app.route("/activate/course", methods=['POST'])
+def activateCourse():
+    authorizedUser = AuthorizedUser()
+    if authorizedUser.isAdmin():
+        data = request.form
+        Course = BannerCourses.get(BannerCourses.reFID == data['activate'])
+        Course.is_active = True
+        message = Course.subject.prefix + ' ' + Course.number + ' ' + Course.ctitle + ' has been activated'
+        Course.save()
+        flash(message)
+        return redirect(redirect_url())
     else:
         abort(403)
