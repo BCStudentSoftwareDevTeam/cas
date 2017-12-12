90dc5f09eb91d4b8eb49149383a441a3f9b695aa
Cody Myers <codymyers63@gmail.com>
2017-12-12 21:02:18 +0000

    Edited the add course functionality to match adding new special topics to bannercourses

--- app/addCourse.py
+++ app/addCourse.py
@@ -18,13 +18,10 @@ adds the course to the course table and to the course change if needed
 @must_be_authorized
 def addCourses(tid, prefix):
     current_page = "/" + request.url.split("/")[-1]
-
-    # set the current page
-
-    # get the data
     data = request.form
-    # instructors need to be a list
+    # # instructors need to be a list
     instructors = request.form.getlist('professors[]')
+    print instructors
 
     nullCheck = NullCheck()
 
@@ -32,9 +29,37 @@ def addCourses(tid, prefix):
     banner = BannerCourses.get(BannerCourses.reFID == values['bannerRef'])
     bannerNumber = str(banner.number)[-2:]
     cId = ""
+    if (bannerNumber == "86" and banner.ctitle == "Special Topics"):
+        specialTopicCourse = SpecialTopicCourse(bannerRef=values['bannerRef'],
+                    prefix=values['prefix'],
+                    term=int(tid),
+                    schedule=values['schedule'],
+                    capacity=values['capacity'],
+                    specialTopicName=values['specialTopicName'],
+                    notes=values['requests'],
+                    crossListed=int(data['crossListed']),
+                    rid=values['rid'],
+                    status = 0,
+                    credits = data['credits'],
+                    description = data['description'],
+                    prereqs = data['prereqs'],
+                    majorReqsMet = data['majorReqsMet'],
+                    concentrationReqsMet = data['concentrationReqsMet'],
+                    minorReqsMet = data['minorReqsMet'],
+                    perspectivesMet = data['perspectivesMet'],
+                    section = data['section'])
+        if data['formBtn'] == "submit":
+            specialTopicCourse.status = 1
+        specialTopicCourse.save()
+        databaseInterface.addSTCourseInstructors(instructors, specialTopicCourse.stId)
 
-    if bannerNumber != "86":
-        # update the course
+        if bannerNumber == "86" and data['formBtn'] == "save":
+            message = "Course: #{0} has been saved. It needs to be submitted before it can be approved.".format(specialTopicCourse.stId)
+            flash("Course has been saved. It needs to be submitted before it can be approved.")
+            log.writer("INFO", current_page, message)
+        else:
+            flash("Course has successfully been added!")
+    else:
         section_exists = Course.select().where(Course.bannerRef == values['bannerRef']).where(Course.term == int(tid)).where(Course.section ==values['section']).exists()
         if section_exists:
             message = "Course: TID#{0} prefix#{1} with section {2} exists".format(tid,prefix, values["section"])
@@ -65,41 +90,9 @@ def addCourses(tid, prefix):
             message = "Course: #{0} has been added".format(course.cId)
             flash("Course has successfully been added!")
             log.writer("INFO", current_page, message)
-    else:
-        specialTopicCourse = SpecialTopicCourse(bannerRef=values['bannerRef'],
-                        prefix=values['prefix'],
-                        term=int(tid),
-                        schedule=values['schedule'],
-                        capacity=values['capacity'],
-                        specialTopicName=values['specialTopicName'],
-                        notes=values['requests'],
-                        crossListed=int(data['crossListed']),
-                        rid=values['rid'],
-                        status = 0,
-                        credits = data['credits'],
-                        description = data['description'],
-                        prereqs = data['prereqs'],
-                        majorReqsMet = data['majorReqsMet'],
-                        concentrationReqsMet = data['concentrationReqsMet'],
-                        minorReqsMet = data['minorReqsMet'],
-                        perspectivesMet = data['perspectivesMet'],
-                        section = data['section']
-        )
+    return redirect(redirect_url())         
 
-        if data['formBtn'] == "submit":
-            specialTopicCourse.status = 1
-
-        specialTopicCourse.save()
-        databaseInterface.addSTCourseInstructors(instructors, specialTopicCourse.stId)
-
-        if bannerNumber == "86" and data['formBtn'] == "save":
-            message = "Course: #{0} has been saved. It needs to be submitted before it can be approved.".format(specialTopicCourse.stId)
-            flash("Course has been saved. It needs to be submitted before it can be approved.")
-            log.writer("INFO", current_page, message)
-        else:
-            flash("Course has successfully been added!")
-
-    return redirect(redirect_url())
+ 
 
 @app.route("/addOne/<tid>", methods=["POST"])
 def add_one(tid):
@@ -183,36 +176,36 @@ def term_courses(term, department):
 
 @app.route("/courses/get_sections/", methods=["POST"])
 def get_sections():
-    course = request.json['course']
-    term = request.json['term']
-    try:
-        term = int(term)
-    except ValueError:
-	return jsonify(list())
-    edit = False
-    if "edit" in request.json:
-        edit = True
-    prefix, number, section = course.split(" ", 2)
-    bannerCourse = BannerCourses.select().where(BannerCourses.subject == prefix).where(BannerCourses.number == number)
-    if bannerCourse.exists():
-        bRef = bannerCourse.get().reFID
-        current_courses = Course.select().where(Course.bannerRef == bRef).where(Course.term == term)
-        if "86" in number:
-            current_courses = SpecialTopicCourse.select().where(SpecialTopicCourse.bannerRef == bRef).where(SpecialTopicCourse.term == term)
-        existing_section = list()
-        sections = list()
-        if edit and section is not None:
-            sections.append(section)
-            existing_section.append(section)
+#     course = request.json['course']
+#     term = request.json['term']
+#     try:
+#         term = int(term)
+#     except ValueError:
+# 	return jsonify(list())
+#     edit = False
+#     if "edit" in request.json:
+#         edit = True
+#     prefix, number, section = course.split(" ", 2)
+#     bannerCourse = BannerCourses.select().where(BannerCourses.subject == prefix).where(BannerCourses.number == number)
+#     if bannerCourse.exists():
+#         bRef = bannerCourse.get().reFID
+#         current_courses = Course.select().where(Course.bannerRef == bRef).where(Course.term == term)
+#         if "86" in number:
+#             current_courses = SpecialTopicCourse.select().where(SpecialTopicCourse.bannerRef == bRef).where(SpecialTopicCourse.term == term)
+#         existing_section = list()
+#         sections = list()
+#         if edit and section is not None:
+#             sections.append(section)
+#             existing_section.append(section)
 
-        for course in current_courses:
-            existing_section.append(course.section)
-        if "A" not in existing_section or len(current_courses) == 0:
-            return jsonify(list("A"))
-        else:
-            sections = generate_sections(existing_section)
-            return jsonify(sections)
-
+#         for course in current_courses:
+#             existing_section.append(course.section)
+#         if "A" not in existing_section or len(current_courses) == 0:
+#             return jsonify(list("A"))
+#         else:
+#             sections = generate_sections(existing_section)
+#             return jsonify(sections)
+    return jsonify(list("A"))
 
 
 def generate_sections(existing_section):
