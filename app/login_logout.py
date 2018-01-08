aef53c7918b21c6da504e2dad7d28d2e53aa0816
Cody Myers <codymyers63@gmail.com>
2018-01-08 17:40:03 +0000

    fixed the login controller.py

--- app/login_logout.py
+++ app/login_logout.py
@@ -1,16 +1,17 @@
 from allImports import *
-
 from app.logic.redirectBack import redirect_url
+from flask import session
+
 @app.route("/login", methods=["GET"])
 def login():
     # get the user from shibboleth
-        system_user = authUser(request.environ)
-        
-        # look for user in our database
+    system_user = authUser(request.environ)
+    # look for user in our database
+    try:
         user = User.get(User.username == system_user)
-        if user is None:
-            abort(403)
-        else:
-            login_user(user)
-            print user.username
+        print user.username
+        login_user(user)
         return redirect(redirect_url())
+    except Exception as e:
+        abort(401)
+    abort(401)
