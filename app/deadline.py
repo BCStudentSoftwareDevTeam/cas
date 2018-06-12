6844adf9b11f8a4f3f35bf4955dc07906cd4e99d
Kat Adams <adamska@berea.edu>
2018-06-12 18:08:52 +0000

    Added a deadline input box to the front page, with supporting controllers (deadline.py) and templates

--- app/deadline.py
+++ app/deadline.py
@@ -4,10 +4,12 @@ import datetime
 from app.logic.redirectBack import redirect_url
 from app.logic.authorization import can_modify
 from flask import session
+import datetime
 
 @app.route("/", methods=["GET"])
 @can_modify
 def deadlineDisplay(can_edit):
+    today = datetime.date.today()
     for key in session.keys():
         session.pop(key)
     terms = Term.select().order_by(-Term.termCode)
@@ -17,13 +19,36 @@ def deadlineDisplay(can_edit):
                            allTerms = terms,
                            can_edit=g.user.isAdmin,
                            currentTerm=int(tid),
-                           deadlines=dates)
+                           deadlines=dates,
+                           today = today)
+
+@app.route("/deadline/create", methods=["POST"])
+def deadlineCreate():
+    # page = "/" + request.url.split("/")[-1]
+    
+    data = request.form
+    
+    # date = datetime.datetime.strptime(data['deadlineDate'],"%m/%d/%Y").date()
+    date = datetime.date.today()
+    
+    deadline = Deadline.create(
+        description=data['deadlineDescription'],
+        date=date)
+    deadline.save()
+
+    message = "Deadline: {0} has been added".format(deadline.description)
+    # log.writer("INFO", page, message)
+    flash("Your Deadline has been created")
+    return redirect(redirect_url('/'))
+
 
 @app.route("/deadline/edit", methods=["POST"])
 def deadlineEdit():
+
     try: 
         data = request.form
-        deadline = Deadline.get(Deadline.id == data['id'])
+        
+        deadline = Deadline.get(Deadline.id == data['id']) 
         deadline.description = data['deadlineDescription']
         deadline.save()
 
