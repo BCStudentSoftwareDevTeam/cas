369cce0eeab4f4b72035b79b82f4b210b2ddb974
awyoonis <jamaalmohammad123@gmail.com>
2018-06-28 19:59:20 +0000

    Finished the html files and ready to start python files

--- app/roomPreference.py
+++ app/roomPreference.py
@@ -4,6 +4,15 @@ from app.logic.redirectBack import redirect_url
 from app import app
 
 @app.route("/roomPreference", methods = ["GET"])
+
+# @save_last_visited
+# @can_modify
+
 def roomPreference():
+    page="Room Preference"
+    # users = User.select().order_by(User.lastName)
+    # rooms = Rooms.select().order_by(Rooms.building)
+   
+    
     return render_template("roomPreference.html")
     
