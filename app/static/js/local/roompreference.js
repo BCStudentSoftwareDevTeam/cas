809dd0005b5ac51597babc38e9f5379b3dd2312f
awyoonis <jamaalmohammad123@gmail.com>
2018-07-05 17:04:46 +0000

    changes in roompreference.html

--- app/static/js/local/roompreference.js
+++ app/static/js/local/roompreference.js
@@ -14,21 +14,45 @@ $(function() {
         
       }
     });
-
        $( "#dialog-modal" ).show();
     });
     
  });
     
-
-
-
- $(document).ready(function(){
-        $("#txt_name").keyup(function(){
-            alert($(this).val());
-        });
-    })
+function validateForm() {
+    var x = document.forms["myForm"].value;
+    if (x != "") {
+        alert("the notes have been saved");
+        return false;
+    }
+}
     
     
     
-// ui-button-icon-primary 
+    
+    
+    
+// //////////////////////////////////////////////////////////////////////////////    
+// function opentextarea() {
+//     var input = document.createElement('textarea');
+//     input.name = 'post';
+//     input.maxLength = 5000;
+//     input.cols = 20;
+//     input.rows = 10;
+//     input.className = 'myCustomTextarea';
+//     var oBody = document.getElementById("body");
+//     while (oBody.childNodes.length > 0) {
+//         oBody.removeChild(oBody.childNodes[0]);
+//     }
+//     oBody.appendChild(input);
+//     document.body.appendChild(button)
+//  }
+
+// function editBox(){
+//     var note;
+//     var note=prompt("Please enter your notes below");
+//     if (note != null){
+//         alert("You edited your notes !");
+//     }
+// }
+
