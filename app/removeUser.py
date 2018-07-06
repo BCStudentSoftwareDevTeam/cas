f5da45ca529ec6454156fb225ebb1e9ffdf41cb9
Anne Otieno <otienoa@berea.edu>
2018-07-06 18:51:54 +0000

    updates to databse,models,config,__init_ and added our appropriate js and conrtoller files

---
+++ app/removeUser.py
@@ -0,0 +1,41 @@
+from allImports import *
+import json
+from playhouse.shortcuts import model_to_dict, dict_to_model
+#Handles populating the appropriate names depending on the specific program/division/building selection in 
+#User Management UI.
+
+
+@app.route('/get_program_chairs/<program>', methods = ["GET"])
+def program_chair(program):
+    allchairs = ProgramChair.select().where(ProgramChair.pid==program)
+    newchairs={}
+    for chair in allchairs:
+        newchairs[chair.username.username]={'firstname':chair.username.firstName,
+                        'lastname':chair.username.lastName,
+                        'username':chair.username.username
+        }        
+    return json.dumps(newchairs)
+    
+    
+@app.route('/get_division_chairs/<division>', methods = ["GET"])
+def division_chair(division):
+#returns all users for a specific division to ajax call when removing users from a specific division in User Management UI
+    allchairs = DivisionChair.select().where(DivisionChair.did==division)
+    newchairs={}
+    for dchair in allchairs:
+        newchairs[dchair.username.username]={'firstname':dchair.username.firstName,
+                        'lastname':dchair.username.lastName,
+                        'username':dchair.username.username
+        }        
+    return json.dumps(newchairs)
+ 
+@app.route('/get_building_managers/<building>', methods = ["GET"])  
+def building_manager(building):
+    allmanagers = BuildingManager.select().where(BuildingManager.bmid==building)
+    newmanagers={}
+    for bmanager in allmanagers:
+        newmangaers[bmanager.username.username]={'firstname':bmanager.username.firstName,
+                        'lastname':bmanager.username.lastName,
+                        'username':bmanager.username.username
+        }
+    return json.dumps(newmanagers)
---
+++ app/static/js/local/removeUser.js
@@ -0,0 +1,165 @@
+/* global $ */
+console.log("removeUser js loaded")
+
+//Shows appropriate 2nd dropdown based on first dropdown selection
+function show_access_level(s) {
+        console.log(s.value);
+        $("#Programs").hide();
+        $("#Divisions").hide();
+        $("#Buildings").hide();
+        $("#Add").hide();
+        $("#Remove").hide();  
+    
+        if (s.value == "program_chair"){
+            $("#Programs").show();
+        }
+        else if (s.value == "division_chair"){
+            $("#Divisions").show();
+        }
+        else if(s.value == "building_manager"){
+            $("#Buildings").show();
+        }
+        else if(s.value == "administrator"){
+            show_names(s);
+        }
+    }
+
+//Program chair remove field
+function fillProgramChairs(response){
+    console.log(response)
+    var programselect = document.getElementById("RemoveDropdown");
+    $("#RemoveDropdown").empty();
+    for (var key in response){
+        // console.log(response[key]['firstname']);
+        var option = document.createElement("option");
+        option.text=response[key]["firstname"].toString()+" "+response[key]["lastname"].toString()+" "+response[key]["username"].toString();
+        option.value = key;
+        programselect.appendChild(option);
+    }
+    // $('.selectpicker').selectpicker('refresh');
+}
+
+function retrievePrograms(obj){
+     var selected_program = obj.value;
+     console.log("Selected program: " + selected_program)
+     if(selected_program){
+        
+         var url = '/get_program_chairs/'+selected_program;
+         console.log("URL: " + url);
+         $.ajax({
+                url: url,
+                dataType: 'json',
+                success: function(response){
+                    console.log(response)
+          		    fillProgramChairs(response);
+          			},
+          			error: function(error){
+          				console.log(error); 
+          			}
+                }); }
+}
+
+function program_chairs_show_names(s) { //Called in HTML
+            retrievePrograms(s);
+            $("#Add").show();
+            $("#Remove").show();
+}
+//Division Chair remove field
+function fillDivisionChairs(response){
+    console.log(response)
+    var divisionselect = document.getElementById("RemoveDropdown");
+    $("#RemoveDropdown").empty();
+    for (var key in response){
+        // console.log(response[key]['firstname']);
+        var option = document.createElement("option");
+        option.text=response[key]["firstname"].toString()+" "+response[key]["lastname"].toString()+" "+response[key]["username"].toString();
+        option.value = key;
+        divisionselect.appendChild(option);
+    }
+    // $('.selectpicker').selectpicker('refresh');
+}
+function retrieveDivisions(obj){
+    console.log(obj.value)
+     var selected_division = obj.value;
+     console.log("Selected division: " + selected_division)
+     if(selected_division){
+        
+         var url = '/get_division_chairs/'+selected_division;
+         console.log("URL: " + url);
+         $.ajax({
+                url: url,
+                dataType: 'json',
+                success: function(response){
+                    console.log(response)
+          		    fillDivisionChairs(response);
+          			},
+          			error: function(error){
+          				console.log(error); 
+          			}
+                }); }
+}
+ function division_chairs_show_names(s) { //Called in html
+            retrieveDivisions(s);
+            $("#Add").show();
+            $("#Remove").show();
+        }
+        
+//Building Manager Remove Field
+function fillBuildingManagers(response){
+    console.log(response)
+    var buildingselect = document.getElementById("RemoveDropdown");
+    $("#RemoveDropdown").empty();
+    for (var key in response){
+        // console.log(response[key]['firstname']);
+        var option = document.createElement("option");
+        option.text=response[key]["firstname"].toString()+" "+response[key]["lastname"].toString()+" "+response[key]["username"].toString();
+        option.value = key;
+        buildingselect.appendChild(option);
+    }
+    // $('.selectpicker').selectpicker('refresh');
+}
+ 
+
+function retrieveBuildings(obj){
+    console.log(obj.value)
+     var selected_building = obj.value;
+     console.log("Selected division: " + selected_building)
+     if(selected_building){
+        
+         var url = '/get_building_managers/'+selected_building;
+         console.log("URL: " + url);
+         $.ajax({
+                url: url,
+                dataType: 'json',
+                success: function(response){
+                    console.log(response)
+          		    fillBuildingManagers(response);
+          			},
+          			error: function(error){
+          				console.log(error); 
+          			}
+                }); }
+}
+
+function building_managers_show_names(s) { 
+            console.log("Got to show names")
+            retrieveBuildings(s);
+            $("#Add").show();
+            $("#Remove").show();
+        }
+
+//FIXME should be function that shows current admins for removal dropdown
+// function administrators_show_names(s) { //TODO: Call this in html 
+//     console.log(s.value);  //TODO: add to py file
+//      if (s.value =="administrator"){
+//          var x=$("#RemoveDropdown");
+         
+         
+//      }
+//     $("#Add").show();
+//     $("#Remove").show();
+// }
+
+
+       
+
