77477e6a884ad2fe6e8c8337e0e4fb4dcd8034b6
Gerardo Soto <sotog@berea.edu>
2018-08-02 15:34:57 +0000

    Got redirect button working properly

--- app/static/js/local/roomResolutionView.js
+++ app/static/js/local/roomResolutionView.js
@@ -9,45 +9,46 @@ $(document).on("click", ".assignroombutton", function () {
      console.log('this is roomID'+roomID);
      var linktoroom = document.getElementById("hiddenroom"+roomID);
      var linktocourse = document.getElementById("hiddencourse");
-    
      $("#assignroomdiv").html("Are you sure you would like to assign "+ linktoroom.value + " to "+linktocourse.value); 
-     
     //  console.log($("#assignroomdiv"));
 });
 
  //Updating assign room modal for preferences tabs
 $(document).on("click",".assignprefbutton", function () {
     console.log("Inside modal update for preferences")
-    var prefID = $(this).data('id'); //Preference ID (1,2,or3)
+    var prefID = $(this).data('id');                                    //Preference ID (1,2,or3)
     //console.log(prefID);
-    var main_course = window.location.href.split("/").pop();
+    var main_course = window.location.href.split("/").pop();            //Grabbing Course A's ID
     //console.log("main course: " + main_course)
     ogCourse = "";
-    var conflictingcourse = document.getElementById("pref"+ prefID +"_confcourse");
-    //console.log("is conflictingcourse null?",conflictingcourse == null); //null
-    if (conflictingcourse != null){
+    var conflictingcourse = document.getElementById("pref"+ prefID +"_confcourse"); //Seeing if there is a conflicting course
+    //console.log("is conflictingcourse null?",conflictingcourse == null);
+    if (conflictingcourse != null){                                     //If there is a conflicting course set it to ogCourse
         conflictingcourse = conflictingcourse.value;
         console.log("conflicting course is this:" + conflictingcourse)
         ogCourse = conflictingcourse;
+        console.log('OGCOURSE',ogCourse)
     }
     //console.log("pref" + prefID + "_room")
-    var room = document.getElementById("pref" + prefID + "_room").value
+    var room = document.getElementById("pref" + prefID + "_room").value //Pulls room from hidden value from html
     //console.log("room",room)
     roomID = room
     //console.log("room:"+ room)
-    var which_preference = document.getElementById("hidden"+ room) //Which preference you have selected
+    var which_preference = document.getElementById("hidden"+ room)       //Which preference you have selected
     // console.log("which_preference.value"+which_preference.value)
     // console.log("hidden"+prefID)
     var linktopref = document.getElementById("hidden"+room);
-   // console.log("link to pref"+linktopref);
+    // console.log("link to pref"+linktopref);
     //console.log("linktopref.value"+linktopref.value);
-    var linktocourse = document.getElementById("hiddencourse"); //Course A
-    
+    var linktocourse = document.getElementById("hiddencourse");         //Course A
+        
     console.log("Link to course"+linktocourse)
-    $("#assignroomdiv").html("Are you sure you would like to assign "+ which_preference.value + " to "+linktocourse.value + "?"); //Need course name
+    $("#assignroomdiv").html("<p>Are you sure you would like to assign "+ which_preference.value + " to "+linktocourse.value + "?</p>"); //Need course name
      
      console.log($("#assignroomdiv").innerHTML);
 });
+
+
 function submitorreplace(){ //Checks if it should be a submit in db or a replace
 
         if (ogCourse == ""){
@@ -151,28 +152,14 @@ function resolvecourse(){ //Functionality for blue redirect button
                 
         });}
         
-//FIXME: make redirect to ogCourses's cid!!!!!!!!        
-function resolvenextcourse(){ //Redirect user to fix the course that was displaced in the assignment
-    var oldurl = window.location.href.split("/");
-    var cid = oldurl[oldurl.length-1];
-    //console.log()
-    var termcode = oldurl[oldurl.length-2];
-    //console.log('Got inside resolvenextcourse')
-    resolvecourse();
-    //console.log('I replaced that there course in that there room fam')
-}
-
 
 function checkforbluebutton(id){//see if blue button needs to show up
     var button = document.getElementById('redirectbutton');
     var check0 = document.getElementById(id);
     if (!!check0){
         $("#redirectbutton").show();
-        
     }
     else{
         $("#redirectbutton").hide();
     }   
-    
-     
 }
