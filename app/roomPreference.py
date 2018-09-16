3ed7a9d86189f2ead480d1f936f77f40e4da1d03
Scott Heggen <heggens@berea.edu>
2018-09-16 17:33:45 +0000

    Finally got the roomPreferences UI to filter by term

--- app/roomPreference.py
+++ app/roomPreference.py
@@ -16,23 +16,28 @@ def roomPreference(term):
     current_user = AuthorizedUser().getUsername()
 
     # Used to populate dropdowns and stuff
-    room = Rooms.select().join(Building, on = (Building.bID == Rooms.building))
+    room = Rooms.select().join(Building, on = (Building.bID == Rooms.building)).order_by(Building.bID.desc(), Rooms.number.desc())
     users= User.select()
     instructors = InstructorCourse.select()
     educationTech= EducationTech.select()
 
     # FIXME used for conflicting courses UI, which is hidden
-    print(current_user)
-    print(current_term)
+    # print(current_user)
+    # print(current_term)
     #Select * from (course) join instructorcourse on instructorcourse.course_id == course.cId and instructorcourse.username_id== 'heggens'  
     term = Term.get(Term.termCode == current_term)
-    print(term)
-    courses = Course.select().join(InstructorCourse, on= (InstructorCourse.course == Course.cId)).where(InstructorCourse.username == current_user)#.where(Course.term.termCode == int(current_term))
+    # print(term)
+    courses = ( Course.select()
+                    .join(InstructorCourse, on= (InstructorCourse.course == Course.cId))
+                    .join(Term, on=(Term.termCode == Course.term))
+                    .where(InstructorCourse.username == current_user)
+                    .where(Course.term == int(current_term))
+                )
     #courses = InstructorCourse.select().where(InstructorCourse.username == current_user).where(InstructorCourse.course_id.term.termCode == int(current_term))
-    for course in courses:
-        # print("Hi Sher")
-        print(course.cId)
-    # for rp in roompreferences:
+    # for course in courses:
+    #     print("Hi Sher")
+    #     print(course.cId)
+    # # for rp in roompreferences:
     #     print(rp.course.cId)
     # roomPreferences = {}
     
@@ -40,9 +45,19 @@ def roomPreference(term):
     # Constructs RoomPreferences if they don't exist
     for course in courses:
         # print("adding ", course.cId, "to ", current_user)
-        RoomPreferences.get_or_create(course = course.cId)
+        (rp, c) = RoomPreferences.get_or_create(course = course.cId)
+        # print(rp.course.term.termCode)
     
-    roompreferences= RoomPreferences.select().join(InstructorCourse, on = (InstructorCourse.course == RoomPreferences.course)).where(RoomPreferences.course == InstructorCourse.course and InstructorCourse.username == current_user).distinct()
+    roompreferences= (  RoomPreferences.select()
+                                    .join(InstructorCourse, 
+                                        on = (InstructorCourse.course == RoomPreferences.course))
+                                    .join(Course, on = (RoomPreferences.course == Course.cId))
+                                    .join(Term, on = (Course.term == Term.termCode))
+                                    .where(RoomPreferences.course == InstructorCourse.course 
+                                            and InstructorCourse.username == current_user)
+                                    .where(Course.term == current_term)
+                                    .distinct()
+                        )
     # roompreferences = RoomPreferences.select().join(Course, on = (RoomPreferences.course == Course.cId)).join(InstructorCourse, on=(Course.cId == InstructorCourse.course)).where(InstructorCourse.username == current_user and Course.term == current_term).distinct()
   
     return render_template(
