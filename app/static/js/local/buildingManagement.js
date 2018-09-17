f45383e833f7ce095b6e8ca0d3a5dfa6add061be
Kat Adams <adamska@berea.edu>
2018-09-17 18:46:55 +0000

    accordion is stuck at bottom now. trying to pull room id

--- app/static/js/local/buildingManagement.js
+++ app/static/js/local/buildingManagement.js
@@ -4,22 +4,36 @@ console.log("Javascript loaded!")
 
 var rIDGlobal = "";
 
+// //added by skrt skrt group
+// function set_datetime(){
+//     var date_time = new Date(2018, 11, 24, 10);
+//     document.getElementById("datetime").innerHTML = date_time.toDateString();
+// }
+
+//end added by skrt skrt group
+
 function setRoomId(rid){
     rIDGlobal=parseInt(rid);
 }
+
 function getRoomId(){
     return rIDGlobal;
 }
 
 function setRoomPanel(){ //Sets room ID based on what room (row) Edit button was clicked
-    //TODO: Pull RID of room that was clicked (each row) to save the data changes to the appropriate room
+    //TODO: Pull RID of room that was clickd (each row) to save the data changes to the appropriate room
+    console.log("setRoomPanel is called")
+    //var roomID = setRoomId($("#selectedRoom").val());
+    
+    console.log("RoomID:", roomID )
     // TODO:Add to this function so that it can show the  collapse and expand level for each room.
-    moveModal()
+    moveModal();
+    $("#roomDetails #Details").hide();
 }
 
-function moveModal(rid) { //Makes dropdown appear between rows
+function moveModal(rID) { //Makes dropdown appear between rows
     var targetDiv = document.getElementById("modalRowRoom"+getRoomId());// hidden row where content will be placed
-    var sourceDiv = document.getElementById("roomData");// content to be placed in targetDiv
+    var sourceDiv = document.getElementById("roomDetails");// content to be placed in targetDiv
     var targetDivs = $(".hiddenRow .hiddenDiv");// all hidden row divs (must be cleared first)
     
     for (var i = 0; i < targetDivs.length; i++) {
@@ -29,10 +43,16 @@ function moveModal(rid) { //Makes dropdown appear between rows
     $(targetDiv).collapse('show');
     //fixSelectPicker();
     //$("#selectedRoom").selectpicker('refresh');// must refresh or causes UI issues
+    
+    
 }
 
 function saveChanges(){ //Posts data to DB and reloads the page
+    // var submitChangesButton = document.getElementById("submitChanges");
+    document.getElementById("datetime").innerHTML = new Date(2018, 11, 24).toDateString();
     
+    //TODO: Ajax call? The page won't need to reload...
+    //Some sort of call to post to the data base!
 }
 
     
