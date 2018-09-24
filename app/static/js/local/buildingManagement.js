9b78e9cecc0aef5fa00bfbc76bce8a363150ba65
Kat Adams <adamska@berea.edu>
2018-09-24 17:59:13 +0000

    Got panel to open up between rows

--- app/static/js/local/buildingManagement.js
+++ app/static/js/local/buildingManagement.js
@@ -13,35 +13,53 @@ function getRoomId(){
 }
 
 function setRoomPanel(roomID, button){ //Sets room ID based on what room (row) Edit button was clicked
-    console.log("setRoomPanel is called")
-    var RoomID = setRoomId($("#selectedRoom").val());
+    setRoomId(roomID);
     console.log("RoomID:", roomID )
-    moveModal(roomID);
-    $("#roomDetails #Details").hide();
+    movePanel(roomID);
+    //$("#roomDetails #Details").hide();
+    var roomNumber = document.getElementById("roomNumber");
+    
 }
 
-function moveModal(rID) { //Makes dropdown appear between rows
-    var targetDiv = document.getElementById(""+getRoomId());// hidden row where content will be placed
+function movePanel(rID) { //Makes dropdown appear between rows
+    var targetDiv = document.getElementById("hiddenRow_"+getRoomId());// hidden row where content will be placed
+    console.log("Target");
+    console.log("hiddenRow_"+getRoomId());
     var sourceDiv = document.getElementById("roomDetails");// content to be placed in targetDiv
-    var targetDivs = $(".hiddenRow .hiddenDiv");// all hidden row divs (must be cleared first)
-    
-    for (var i = 0; i < targetDivs.length; i++) {
-        $(targetDivs[i]).empty();// empty all the hiddenRows
-    }
+    // var targetDivs = $(".hiddenRow .hiddenDiv");// all hidden row divs (must be cleared first)
+    // for (var i = 0; i < targetDivs.length; i++) {
+    //     $(targetDivs[i]).empty();// empty all the hiddenRows
+    // }
     $(targetDiv).html($(sourceDiv)); // moves modal content into current row
-    $(targetDiv).collapse('show');
+    $(sourceDiv).collapse('show');
     //fixSelectPicker();
     //$("#selectedRoom").selectpicker('refresh');// must refresh or causes UI issues
-    
-    
 }
 
 function saveChanges(){ //Posts data to DB and reloads the page
     // var submitChangesButton = document.getElementById("submitChanges");
     document.getElementById("datetime").innerHTML = new Date(2018, 11, 24).toDateString();
     
-    //TODO: Ajax call? The page won't need to reload...
-    //Some sort of call to post to the data base!
-}
+    //TODO: FIX AJAX CALL TO POST CHANGES TO DB
+//     $.ajax({
+//          type: "POST",
+//             url: url,
+//             data: {"": },
+//             dataType: 'json',
+//             success: function(response){ 
+//                 console.log(response)
+//                 console.log("I updated the room data")
+//                  if (response['success'] == 1)
+//                     window.location = "/buildingManagement"
+//                     else
+//                     window.location.assign("/buildingManagement")                    
+//       			},
+//       			error: function(error){
+//       			    console.log("It didnt work")
+//       				console.log(error); 
+//                     window.location.assign("/buildingManagement")
+//       			}
+//     }
+ }
 
     
