92cdbfbc026a443f1b8a5e5d723d819f1880546c
Kenny Nelson <nelsonk@berea.edu>
2018-09-24 20:04:21 +0000

    Fixed Reverse functionalities and did some code editing/review'

--- app/static/js/local/systemManagement.js
+++ app/static/js/local/systemManagement.js
@@ -1,38 +1,67 @@
 var lastTerm = "";
 function showPanel(termCode, button){
+    
     var show_id = button.dataset.target;    // The target panel for the term (e.g., Fall 2018)
+    
     var disable_btn = document.getElementsByClassName("theButtons"); // All of the state buttons 
+    
     disable_btn.disabled = true;            // Disables all the state buttons
+    
     button.disabled = false;                // Disables the term button
+    
     // console.log("Term code: " + termCode);
+    
     var targetDiv = $("#divForPanel"+termCode);  // The target location for the panel (which row to put it under)
+
     // console.log("target div: " + targetDiv);
+    
     var subjectDiv = $("#allPanels");           // The panel itself, to be moved
+    
     // console.log("subject div: " + subjectDiv);
+    
     // subjectDiv.attr("hidden", false);
+    
     targetDiv.html(subjectDiv);                 // Move the panel
+    
     // Hides all the panels and removes any styling
+    
     for (var i = 0; i <= 7; i++ ){ 
+    
         var build_id = '#order' + i.toString();
+    
         $(build_id).collapse('hide');
+    
         $(build_id).removeAttr("style")
 
     }
+    
     // console.log(show_id);
+    
     // $(show_id).collapse('show');
+    
     if (lastTerm == "") {               // if this is the first click (no lastTerm set)
+    
         // console.log("First click")
+    
         $(show_id).collapse('show');    // Show the panel for this term
     }
+    
     if (lastTerm != termCode && lastTerm != "") {       // if the lastTerm selected is the same
+    
         // console.log($(show_id).attr('aria-expanded'))
+    
         $(show_id).collapse('show');    // show the target panel for this term
+    
         // console.log($(show_id).attr('aria-expanded'))
+    
         $(show_id).addClass("in");      
 
         // console.log("Toggling");
     } else {
+     
         $(show_id).collapse('toggle');      // Otherwise, toggle open/closed panel
+    
+        
     }
     // console.log("Last term: ", lastTerm)
     lastTerm = termCode;                // Update lastTerm to the term you're currently on
@@ -41,40 +70,53 @@ function showPanel(termCode, button){
 
 // Disables all the state buttons 
 function disableAlltheButtons(){
+    
     var x = document.getElementsByClassName('theButtons'); 
     // console.log(x);
+    
     for (var i = 0; i < x.length; i++) {
         x[i].disabled = true
+    
         // console.log("Disabled button: " + x[i])
     }
 }
 
 // Enables one state button
 function enableTheOne() {
+    
     // console.log(arguments);
+    
     disableAlltheButtons();
+    
     for (var i = 0; i < arguments.length; i++) {
+    
         // console.log(arguments[i])
+    
         //b = document.getElementById(arguments[i]);           // Grabs the button
+    
         b = $(arguments[i]);
+    
         // console.log("Der button", b);
+    
         b.disabled = false;
     }
 }
 
-// Adds the color for the completed processes
+
 function prevcolor(btn) {
-    // console.log(btn)
-        var allPanelsDiv = $("#allPanels");
-        var theTR = allPanelsDiv.parent().parent()[0].id.split("_").pop();
-        // console.log(theTR);
-        // console.log("color changed");
-        // console.log("#"+theTR +" #" +btn);
-        var remove_color = $("#"+theTR +" #" +btn); //Removes the color of the previous process to ensure there is no overide
-        // console.log(remove_color);
-        remove_color.removeClass("btn-dark");
-        var prev = $("#"+theTR+" #" + btn);
-        prev.addClass('btn-success');
+    // Adds the color for the completed processes
+
+    var allPanelsDiv = $("#allPanels");
+    
+    var theTR = allPanelsDiv.parent().parent()[0].id.split("_").pop();
+    
+    var remove_color = $("#"+ theTR +" #" +btn); //Removes the color of the previous process to ensure there is no overide
+    
+    remove_color.removeClass("btn-dark");
+    
+    var prev = $("#"+ theTR +" #" + btn);
+    
+    prev.addClass('btn-success');
     
 }
 
@@ -82,21 +124,22 @@ function prevcolor(btn) {
  * applicable for two argumnets only for now uness adjusted 
  * @params {HTML Buttons} btns - all the buttons
  */
-function reverseFunc(btn) {
-    // disableAlltheButtons(arguments[btns]);
-    // console.log(arguments[btns]);
-    // var go_back = document.getElementById(arguments[0]);
-    // console.log(go_back)
-    // go_back.disabled = false;
-    // var reverse_btn = document.getElementById(arguments[0]);
-    // reverse_btn.className = 'theButtons btn btn-dark';
+function reverseFunc(btn, StateOrder) {
+    // This function handles what happens when the user chooses to reverse a state. It changes the color of the button and invokes the function that updates the database
+    
     var allPanelsDiv = $("#allPanels");
+    
     var theTR = allPanelsDiv.parent().parent()[0].id.split("_").pop();
-    var remove_color = $("#"+theTR +" #" +btn); //Removes the color of the previous process to ensure there is no overide
-    // console.log(remove_color);
+    
+    var remove_color = $("#"+theTR +" #" +btn); // This is the button whose color will need to be changed from btn-success. It is the button whose action will be reversed
+    
     remove_color.removeClass("btn-success");
+    
     var prev = $("#"+theTR+" #" + btn);
+    
     prev.addClass('btn-dark');
+    
+    submit_data(parseInt(StateOrder)-1, 'true'); // Save the new state to the database through an ajax call
     
 }
 
@@ -132,56 +175,91 @@ window.onload = function() {
 }
 
 function loadStateToTerm(){
-    //This function makes sure that on load we can tell where each term was left off
-    var x = document.getElementsByClassName("terms_btn");
-    var btn_arr = []
+    //This function makes sure that on load we can tell where each term was left off when it comes to their state
+   
+    var x = document.getElementsByClassName("terms_btn"); // Get all the term buttons
+    
+    var btn_arr = [];
 
     for (var i = 0; i < x.length; i++) {
-        var btn_id = x[i].id;
-        btn_arr.push(btn_id)
-        var state = parseInt(btn_id.slice(7)) + 1;
-        $('#'+btn_id).attr('data-target','#order'+state.toString())
-        $('#'+btn_id).attr('aria-controls', 'order'+state.toString())
+        
+        var btn_id = x[i].id; // Get the id of each term button. The id of each term button consists of the termcode and the current state of that term
+        
+        btn_arr.push(btn_id); // Push the id of each term button to the button array
+        
+        var state = parseInt(btn_id.slice(7)) + 1; // Add 1 to the state of each button because the state order column table in the database starts with 0
+        
+        $('#'+btn_id).attr('data-target','#order'+state.toString()); // Update the data target of each term button 
+        
+        $('#'+btn_id).attr('aria-controls', 'order'+state.toString()); // Update the aria-controls of each term button
     }
     for (var i = 0; i < x.length; i++){
-    var term = btn_arr[i].slice(0,6);
-    var state = btn_arr[i].slice(7);
-    var count = parseInt(state);
-    for (var c = 1; c < count +1; c++){
-    var btnToColor = $('#'+term +' #btn'+c.toString());
-    btnToColor.removeClass("btn-dark");
-    btnToColor.addClass('btn-success');
-    }
-    }
+        // This for loop is to color the buttons for the state buttons already achieved
+        
+        var term = btn_arr[i].slice(0,6);
+        
+        var state = parseInt(btn_arr[i].slice(7));
+   
+        for (var c = 1; c < state +1; c++){
+            
+            var btnToColor = $('#'+term +' #btn'+c.toString());
+            
+            btnToColor.removeClass("btn-dark");
+            
+            btnToColor.addClass('btn-success');
+            }
+        }
     }
 
-function updateStateDataTarget(termCode,termState){
-    console.log("This is the termState returned", termState)
-    var next_state = parseInt(termState)+1;        // shift to the next state\
-    var previous_state = (parseInt(termState)-1)
-    var term_btn = $('#' + termCode+'_' + previous_state.toString());
-    var new_id = termCode+'_'+termState.toString()
-   // console.log('Before any update:', term_btn);
-    term_btn.attr("id", new_id);
+function updateStateDataTarget(termCode,termState, reverseStatus){
+    // This function changes the data target of the term button that opens the panel where the user gets to move to the next state or reverse to a previous state
+    
+    var next_state = parseInt(termState) +1;        // the next state
+   
+    if (reverseStatus == 'true'){ // This is to recognize if the user tried to reverse an action
+        
+        var previous_state = (parseInt(termState)+1)
+        
+        var term_btn = $('#' + termCode+'_' + previous_state.toString());
+    }
+    else{
+        
+        var previous_state = (parseInt(termState)-1)
+        
+        var term_btn = $('#' + termCode+'_' + previous_state.toString());
+    }
+  
+    var new_term_btn_id = termCode+'_'+termState.toString()
+  
+    term_btn.attr("id", new_term_btn_id);
+    
     term_btn.attr('data-target','#order'+next_state.toString())
+    
     term_btn.attr('aria-controls', 'order'+next_state.toString())
-   // console.log("After any update", term_btn);
+ 
 }
     
-function submit_data(stateOrder){
+function submit_data(stateOrder, reverseStatus){
+    // This function sends an ajax call to the controller to save the state of a term in the database 
+    
     var allPanelsDiv = $("#allPanels");
+    
     var termCode = allPanelsDiv.parent().parent()[0].id.split("_").pop();
-    // console.log('termcode', termCode)
-    console.log('stateOrder passed', stateOrder)
-    // console.log(theTR)
+   
     $.ajax({
-        url:'/admin/systemManagement/updateTermState',
+        
+        url:'/admin/systemManagement/updateTermState', // This is the link to the controller
+        
         data:{'stateOrder':stateOrder,'termCode':termCode},
+        
         type: "POST",
+        
         cache: false,
+        
         success: function () {
-            console.log('Success')
-            updateStateDataTarget(termCode, stateOrder);
+            //console.log('Success')
+            
+            updateStateDataTarget(termCode, stateOrder, reverseStatus); // On success of the saving to the database, update the data target for the term button
           
         },
         error: function (xhr, ajaxOptions, thrownError) {
@@ -189,7 +267,6 @@ function submit_data(stateOrder){
         }
         
     })
-    
     
 } 
 
@@ -268,34 +345,6 @@ $(document).ready(function(){
         $('[data-toggle="modal"]').modal({show:false}); 
         loadStateToTerm();
 });
-
-// $(window).bind('load', function(){
-//     console.log('Here')
-//     loadStateToTerm();
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // /* global $ */
--- app/systemManagement.py
+++ app/systemManagement.py
@@ -7,59 +7,47 @@ from app.logic.authorization import must_be_admin
 @app.route("/admin/systemManagement", methods=["GET"])
 @must_be_admin
 def systemManagement():
-      terms          = Term.select()
-      users          = User.select()
-      program        = Program.get()
-      today          = datetime.date.today()
+      terms          = Term.select() # Select all the terms for the terms table with the state buttons
+      
+      today          = datetime.date.today() 
+      
       term_state     = TermStates.select().order_by(TermStates.order)
-      #WE WANT THE USER TO HAVE THE ABILITY TO SELECT A YEAR AGO AND THREE YEARS PAST THE CURRENT YEAR
-      years       = []
-      #START WITH ONE YEAR AGO
-     
-      year  = int(time.strftime("%Y")) - 1   
-      for x in range(5):
+      
+      years          = [] #WE WANT THE USER TO HAVE THE ABILITY TO SELECT A YEAR AGO AND THREE YEARS PAST THE CURRENT YEAR
+      
+      year           = int(time.strftime("%Y")) - 1   #START WITH ONE YEAR AGO
+      for x in range (5):
          if x == 0:
-            #APPEND THE ONE YEAR AGO
-            years.append(str(year))          
+            
+            years.append(str(year))  #APPEND THE ONE YEAR AGO        
          year += 1
-         #APPEND CURRENT NUMBER PLUS ONE
-         years.append(str(year))          
+        
+         years.append(str(year))    #APPEND CURRENT NUMBER PLUS ONE       
       return render_template("systemManagement.html",
+                              
                               terms          = terms,
+                              
                               years          = years,
-                              program        = program,
-                              users          = users,
-                              isAdmin        = g.user.isAdmin,
+                              
                               today          = today,
+                              
                               term_state     = term_state)
                               
-#Tomorrow: 
 
-#Then I will start working on the reverse actions which occurs under open room preferences, assign rooms and finish panels
 
-#Thursday and Friday
-# In addition to updating the state for the terms I should also implement functionality that opens/locks scheduling, open/lock room preferences, assign rooms
+
 
 @app.route("/admin/systemManagement/updateTermState", methods=["POST"])
 @must_be_admin
 def updateTermState():
-   data = request.form
-
-   # Updates the term's state to the correct state ID based on state order
-   term = Term.get(Term.termCode == data['termCode'])
-   state = TermStates.get(TermStates.order == data['stateOrder'] )
-   print("This is the state", state.order)
-   # term.state = state.csID
-   print ('state csID', state.csID)
-   term = Term.update({Term.state: state.csID}).where(Term.termCode == data['termCode']).execute()
-   # print("Updated state again", term.state.order)
-   # print("State again id", term.state.csID)
-   # term.save()
-
+   ''' Updates a term's state to the correct state ID based on state order '''
    
+   data   = request.form # collect data from the view
+
+   term   = Term.get(Term.termCode == data['termCode']) # Retrieve from the database the term whose state was changed by the user and that needs to be updated
    
-   # message = "Course: #{0} has been added".format(course.cId)
-   # flash("Course has successfully been added!")
-   # log.writer("INFO", current_page, message)
+   state  = TermStates.get(TermStates.order == data['stateOrder'] ) # Retrive the state that will be associated with the term
+
+   Term.update({Term.state: state.csID}).where(Term.termCode == data['termCode']).execute() # update the state of the specific term retrieved
    
    return redirect(url_for("systemManagement")) 
