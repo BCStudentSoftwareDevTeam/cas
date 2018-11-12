37c4bd91600bed609300ba79bd563b080f1d50e1
Jonathan <chauwaj@berea.edu>
2018-11-12 03:08:18 +0000

    Deleting parent crosslisted courses

--- app/deleteCourse.py
+++ app/deleteCourse.py
@@ -2,6 +2,7 @@ from allImports import *
 from updateCourse import DataUpdate
 from app.logic import databaseInterface
 from app.logic.redirectBack import redirect_url
+from app.logic.course import find_crosslist_courses
 from app.logic.authorization import must_be_authorized
 
 @app.route("/deletecourse/<tid>/<prefix>", methods=["POST"])
@@ -9,7 +10,10 @@ from app.logic.authorization import must_be_authorized
 def deletecourse(prefix, tid):
     
     current_page = "/" + request.url.split("/")[-1]
-
+    #Check if course is crosslisted parent course
+    #find child courses
+    #delete parent course with all child crosslisted courses
+    
 
     # DATA NEEDED FOR MANIPULATION
     # TODO: Change the colors when a course is updated
@@ -18,6 +22,31 @@ def deletecourse(prefix, tid):
     cid = int(data['cid'])
     # START PROCESSING THE DELETION OF THE COURSE
     course = Course.get(Course.cId == cid)
+    
+    #if course is a crosslisted parent course, delete it with all its child courses
+    if course.crossListed == 1 and course.parentCourse == None:
+        crosslistedCourses = CrossListed.select().where(CrossListed.courseId == cid)
+        crosslistedChildCourses = Course.select().where(Course.parentCourse == cid)
+        #delete all related crosslisted courses in crosslisted table
+        for crosslisted_course in crosslistedCourses:
+            crosslisted_course.delete_instance()
+        #delete all related courses child courses in course table
+        for childcourse in crosslistedChildCourses:
+            childcourse.delete_instance()
+    
+    #Needs thinking about
+    # elif:
+    #     course.crosslisted == 1 and course.parentCourse != None:
+    #     crosslistedCourse = CrossListed.select().where(CrossListed.courseId == cid)
+    #     crosslistedChildCourses = Course.select().where(Course.parentCourse == cid)
+        
+            
+
+   
+    
+    # if course.parentCourse_id == None and course.cId == crosslisted:
+    #     print("Printing crosslisted courses here")
+    #     print(course)
     # MAKE SURE THE USER HAS THE CORRECT RIGHTS TO DELETE A COURSE
     if not databaseInterface.isTermOpen(tid):
 
