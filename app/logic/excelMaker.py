4bf33925fb9c6429905cd48e4939034f3a88d2b2
Kenny Nelson <nelsonk@berea.edu>
2019-01-16 15:57:05 +0000

    Adding Room Components to the excel maker

--- app/logic/excelMaker.py
+++ app/logic/excelMaker.py
@@ -21,10 +21,13 @@ class ExcelMaker:
         sheet.write('F1', 'Time')
         sheet.write('G1', 'Capacity')
         sheet.write('H1', 'Notes')
-        sheet.write('I1', 'Assigned Room')
-        sheet.write('J1','Section')
-        sheet.write('K1','Instructors')
-        self.intr_letter = 'K'
+        sheet.write('I1','Section')
+        sheet.write('J1', 'Assigned Room')
+        sheet.write('K1', 'Preference 1')
+        sheet.write('L1','Preference 2')
+        sheet.write('M1','Preference 3')
+        sheet.write('N1','Instructors')
+        self.intr_letter = 'N'
         
     def writeSpecialHeaders(self,sheet):
         sheet.write('K1','credits')
@@ -36,17 +39,72 @@ class ExcelMaker:
         sheet.write('Q1', 'Perspectives Request')
         sheet.write('R1', 'Instructors')
         self.intr_letter = 'R'
+    
+    def writeAllRoomHeaders(self, sheet):
+        sheet.write('A1', 'Room')
+        sheet.write('B1', 'Capacity')
+        sheet.write('C1', 'Seating Layout')
+        sheet.write('D1', 'Visual Accessibility')
+        sheet.write('E1', 'Audio Accessibility')
+        sheet.write('F1', 'Physical Accessibility')
+        sheet.write('G1', 'Specialized Equipment')
+        sheet.write('H1', 'Special Features')
+        sheet.write('I1', 'Movable Furniture')
+        sheet.write('J1', 'Projector(s)')
+        sheet.write('K1', 'SmartBoard(s)')
+        sheet.write('L1', 'Instructor Computers')
+        sheet.write('M1', 'Podium')
+        sheet.write('N1', 'Student Workspaces')
+        sheet.write('O1', 'Chalkboards')
+        sheet.write('P1', 'WhiteBoards')
+        sheet.write('Q1', 'DVD')
+        sheet.write('R1', 'Blu-Ray')
+        sheet.write('S1', 'Audio')
+        sheet.write('T1', 'Extro')
+        sheet.write('U1', 'Doc Cam')
+        sheet.write('V1', 'VHS')
+        sheet.write('W1', 'Mondopad')
+        sheet.write('X1', 'Tech Cart')
+        self.intr_letter = 'X'
+    
+        
+    def write_all_rooms_info(self, sheet, room, row):
+        sheet.write('A{0}'.format(row), room.building.name +' '+ room.number)
+        sheet.write('B{0}'.format(row), room.maxCapacity)
+        sheet.write('C{0}'.format(row), room.roomType)
+        sheet.write('D{0}'.format(row), room.visualAcc)
+        sheet.write('E{0}'.format(row), room.audioAcc)
+        sheet.write('F{0}'.format(row), room.physicalAcc)
+        sheet.write('H{0}'.format(row), room.specializedEq)
+        sheet.write('I{0}'.format(row), room.specialFeatures)
+        sheet.write('J{0}'.format(row), room.movableFurniture)
+        
+        
+        
+        
+        
         
     def write_course_info(self,sheet,row,course):
         # Course Information
         sheet.write('A{0}'.format(row),course.prefix.prefix)
         sheet.write('B{0}'.format(row),course.bannerRef.number)
         sheet.write('C{0}'.format(row),course.bannerRef.ctitle)
+        
+        
         #Course Schedule
         if course.schedule is not None:
             self.writeRow(sheet,'D',row,course.schedule.sid)
             self.writeRow(sheet,'E',row,course.schedule.letter)
-            days = course.schedule.days.get().day
+            # print('About to get days')
+                 
+            schedule_days = ScheduleDays.select().where(ScheduleDays.schedule == course.schedule.sid)
+      
+            days = ""
+            for i in schedule_days:
+                days += str(i.day)
+          
+   
+          
             if days is None:
                 days = "TBD"
             time = days + ': '+ str(course.schedule.startTime) + ' - ' + str(course.schedule.endTime)
@@ -57,14 +115,38 @@ class ExcelMaker:
         # Room Information
         room_name = ""
         if course.rid:
-            room_name = course.rid.building.shortName + ':' + course.rid.number
-        sheet.write('I{0}'.format(row),room_name)
+            room_name = course.rid.building.name + ' ' + course.rid.number
+        sheet.write('J{0}'.format(row),room_name)
+        sheet.write('I{0}'.format(row),course.section)
+        
+        
+        # Room Information
+        room_preferences = RoomPreferences.select().where(RoomPreferences.course == course.cId)
+     
+        preference_1 = ""
+        preference_2 = ""
+        preference_3 = ""
+        
+        if room_preferences: 
+            for room_preference in room_preferences:
+                if room_preference.pref_1:
+                    preference_1 = room_preference.pref_1.building.shortName + " " + room_preference.pref_1.number
+                    sheet.write('K{0}'.format(row),preference_1)
+                if room_preference.pref_2:
+                    preference_2 = room_preference.pref_2.building.shortName + " " + room_preference.pref_2.number
+                    sheet.write('L{0}'.format(row),preference_2)
+                if room_preference.pref_3:
+                    preference_3 = room_preference.pref_3.building.shortName + " " + room_preference.pref_3.number
+                    sheet.write('M{0}'.format(row),preference_3)
+                  
+       
+       
         #Instructor Information
-        if self.intr_letter == 'K':
+        if self.intr_letter == 'N':
             instructors = InstructorCourse.select().where(InstructorCourse.course == course.cId)
         else:
             instructors = InstructorCourse.select().where(InstructorCourse.course == course.course)
-        sheet.write('J{0}'.format(row),course.section)
+       
         colNum = ord(self.intr_letter)
         for  instructor in instructors:
             self.writeRow(sheet,chr(colNum),row,instructor.username.username)
@@ -81,6 +163,10 @@ class ExcelMaker:
         sheet.write('O{0}'.format(row),course.concentrationReqsMet)
         sheet.write('P{0}'.format(row),course.minorReqsMet)
         sheet.write('Q{0}'.format(row),course.perspectivesMet)
+
+        
+
+        
         
     def increment_rows(self,course):
         self.program_row  += 1
@@ -97,34 +183,58 @@ class ExcelMaker:
         'title':    'Course Schedule for {}'.format(term.name),
         'author':   'Cas System',
         'comments': 'Created with Python and XlsxWriter'})
-        self.mater_row = 2
+        self.master_row = 2
         self.cross_row = 2
+        self.room_row = 2
         #Create worksheets and Set Headers
         master_sheet = workbook.add_worksheet('All Courses')
         self.writeHeaders(master_sheet)
 
         cross_sheet = workbook.add_worksheet('CrossListed')
         self.writeHeaders(cross_sheet)
-
+        
+        
+        # Create worksheet for Rooms
+        allrooms_sheet = workbook.add_worksheet('All Rooms')
+        self.writeRoomHeaders(allrooms_sheet, 'ALL')
+        
+        availablerooms_sheet = workbook.add_worksheet('Available Rooms')
+        self.writeRoomHeaders(availablerooms_sheet, 'available')
+        
+        rooms = Rooms.select().order_by(Rooms.building_id)
+        for room in rooms:
+            self.write_all_rooms_info(allrooms_sheet, room, self.room_row)
+            self.room_row += 1
+            
         #Loop through programs
         programs = Subject.select().order_by(Subject.prefix)
+      
+      
+        # Create worksheets and set headers for each program
         for program in programs:
+           
             self.program_row = 2 #reset the program row
             program_sheet = workbook.add_worksheet(program.prefix)
             self.writeHeaders(program_sheet)
-
+                
             #Loop through Courses in that program
             courses = Course.select().where(Course.prefix == program.prefix).where(Course.term == term).order_by(Course.bannerRef)
+            
             for course in courses:
+             
                 sheet_matrix = [[master_sheet,self.master_row],[program_sheet,self.program_row]]
+                # print(course.cId, course.schedule.letter, course.prefix.prefix, sheet_matrix)
                 if course.crossListed:
                     sheet_matrix.append([cross_sheet,self.cross_row])
                 for sheet_list in sheet_matrix:
                     self.write_course_info(sheet_list[0],sheet_list[1],course)
                 self.increment_rows(course)
+        
+      
+       
         workbook.close()
         return path
-
+        
     def make_cross_listed_file(self, term):
         #set excel parameters variables
         filename = "cas-{}-crossListed.xlsx".format(term.termCode)
