7f7613280cd39ab474d427b0fcb68dea343a485d
awyoonis <jamaalmohammad123@gmail.com>
2019-02-04 21:44:39 +0000

    merge conflict

--- app.py
+++ app.py
@@ -31,6 +31,16 @@ else:
 
 
 
+# if os.getenv('IP'):
+#   IP = os.getenv('IP')
+# else:
+#   IP = '0.0.0.0'
+
+# if os.getenv('PORT'):
+#   # pass
+#   PORT = int(os.getenv('PORT'))
+# else:
+#   PORT = 8081
 
 # Print statements go to your log file in production; to your console while developing
 print ("Running server at http://{0}:{1}/".format(IP, PORT))
