6a05869c24a28d64bb38c2a0ba3986fed61e3d0a
Sher Sanginov <sanginovs@berea.edu>
2019-02-28 15:09:26 +0000

    docstrings added

--- app/addCourse.py
+++ app/addCourse.py
@@ -13,73 +13,84 @@ from flask import jsonify
 adds the course to the course table and to the course change if needed
 '''
 
+
 def convertPrereqs(prereqs):
     "prevents from storing empty prereq string into the database"
     if prereqs[0]:
         return prereqs[0]
-    else:
-        return None
-        
-        
+    return None
+
+
 @app.route("/addCourse/<tid>/<prefix>", methods=["POST"])
 @must_be_authorized
 def addCourses(tid, prefix):
     current_page = "/" + request.url.split("/")[-1]
     data = request.form
-   
+
     # # instructors need to be a list
-    
+
     instructors = request.form.getlist('professors[]')
     prereqs = request.form.getlist('prereqs')
-   
+
     nullCheck = NullCheck()
-    
+
     values = nullCheck.add_course_form(data)
-    
+
     banner = BannerCourses.get(BannerCourses.reFID == values['bannerRef'])
     bannerNumber = str(banner.number)[-2:]
-    
+
     cId = ""
-   
+
     if (bannerNumber == "86" and banner.ctitle == "Special Topics"):
-        
-        specialTopicCourse = SpecialTopicCourse(bannerRef=values['bannerRef'],
-                    prefix=values['prefix'],
-                    term=int(tid),
-                    schedule=values['schedule'],
-                    capacity=values['capacity'],
-                    specialTopicName=values['specialTopicName'],
-                    notes=values['requests'],
-                    crossListed=int(data['crossListed']),
-                    rid=values['rid'],
-                    status = 0,
-                    credits = data['credits'],
-                    description = data['description'],
-                    prereqs = data['prereqs'],
-                    majorReqsMet = data['majorReqsMet'],
-                    concentrationReqsMet = data['concentrationReqsMet'],
-                    minorReqsMet = data['minorReqsMet'],
-                    perspectivesMet = data['perspectivesMet'],
-                    section = data['section'])
+
+        specialTopicCourse = SpecialTopicCourse(
+            bannerRef=values['bannerRef'],
+            prefix=values['prefix'],
+            term=int(tid),
+            schedule=values['schedule'],
+            capacity=values['capacity'],
+            specialTopicName=values['specialTopicName'],
+            notes=values['requests'],
+            crossListed=int(
+                data['crossListed']),
+            rid=values['rid'],
+            status=0,
+            credits=data['credits'],
+            description=data['description'],
+            prereqs=data['prereqs'],
+            majorReqsMet=data['majorReqsMet'],
+            concentrationReqsMet=data['concentrationReqsMet'],
+            minorReqsMet=data['minorReqsMet'],
+            perspectivesMet=data['perspectivesMet'],
+            section=data['section'])
         if data['formBtn'] == "submit":
             specialTopicCourse.status = 1
         specialTopicCourse.save()
-        databaseInterface.addSTCourseInstructors(instructors, specialTopicCourse.stId)
+        databaseInterface.addSTCourseInstructors(
+            instructors, specialTopicCourse.stId)
 
         if bannerNumber == "86" and data['formBtn'] == "save":
-            message = "Course: #{0} has been saved. It needs to be submitted before it can be approved.".format(specialTopicCourse.stId)
-            flash("Course has been saved. It needs to be submitted before it can be approved.")
+            message = "Course: #{0} has been saved. It needs to be submitted before it can be approved.".format(
+                specialTopicCourse.stId)
+            flash(
+                "Course has been saved. It needs to be submitted before it can be approved.")
             log.writer("INFO", current_page, message)
         else:
             flash("Course has successfully been added!")
     else:
-        section_exists = Course.select().where(Course.bannerRef == values['bannerRef']).where(Course.term == int(tid)).where(Course.section ==values['section']).exists()
+        section_exists = Course.select().where(
+            Course.bannerRef == values['bannerRef']).where(
+            Course.term == int(tid)).where(
+            Course.section == values['section']).exists()
         if section_exists:
-            message = "Course: TID#{0} prefix#{1} with section {2} exists".format(tid,prefix, values["section"])
+            message = "Course: TID#{0} prefix#{1} with section {2} exists".format(
+                tid, prefix, values["section"])
             log.writer("INFO", current_page, message)
-            flash("Course with section %s already exists" % (values['section']),"error")
+            flash(
+                "Course with section %s already exists" %
+                (values['section']), "error")
             return redirect(redirect_url())
-        
+
         # cross_courses=values["crossListed"]# [1,2,3]
         course = Course(bannerRef=values['bannerRef'],
                         prefix=values['prefix'],
@@ -89,79 +100,85 @@ def addCourses(tid, prefix):
                         specialTopicName=values['specialTopicName'],
                         notes=values['requests'],
                         crossListed=int(data['crossListed']),
-                        section = values['section'],
-                        prereq = convertPrereqs(prereqs)
+                        section=values['section'],
+                        prereq=convertPrereqs(prereqs)
                         )
-                        
+
         course.save()
-        
-        #update coursechange table if term is not editable: 
-        #the changetracker feature will be removed soon 
+
+        # update coursechange table if term is not editable:
+        # the changetracker feature will be removed soon
         newCourse = DataUpdate()
         databaseInterface.addCourseInstructors(instructors, course.cId)
-        if not databaseInterface.isTermOpen(tid):  # IF THE TERM IS NOT EDITABLE
-                # ADD THE COURSE TO THE COURSECHANGE TABLE
-                newCourse.addCourseChange(course.cId, cfg["changeType"]["create"])
-    
-                message = "Course: #{0} has been added".format(course.cId)
-                log.writer("INFO", current_page, message)
-        
-        #save crosslisted courses of the newly-created course in a database
+        if not databaseInterface.isTermOpen(
+                tid):  # IF THE TERM IS NOT EDITABLE
+            # ADD THE COURSE TO THE COURSECHANGE TABLE
+            newCourse.addCourseChange(course.cId, cfg["changeType"]["create"])
+
+            message = "Course: #{0} has been added".format(course.cId)
+            log.writer("INFO", current_page, message)
+
+        # save crosslisted courses of the newly-created course in a database
         if(course.crossListed):
-            create_crosslisted_courses(values, course,  tid, prereqs, instructors)
-        
+            create_crosslisted_courses(
+                values, course, tid, prereqs, instructors)
+
         flash("Course has successfully been added!")
-        return redirect(redirect_url())  
-        
-        
-    
-        
+        return redirect(redirect_url())
+
+
 def create_crosslisted_courses(values, course, tid, prereqs, instructors):
-    crosslistedCourses=values["crossListedCourses"]
+    '''
+    Creates a crosslisted child relationship for a course
+    
+    '''
+    crosslistedCourses = values["crossListedCourses"]
     if crosslistedCourses:
-        
-        #save parent crosslisted to itself
-        crosslisted = CrossListed(courseId = course.cId, crosslistedCourse = course.cId,
-            prefix = course.prefix,
-            verified = True,
-            term=int(tid)
-        )
+
+        # save parent crosslisted to itself
+        crosslisted = CrossListed(
+            courseId=course.cId,
+            crosslistedCourse=course.cId,
+            prefix=course.prefix,
+            verified=True,
+            term=int(tid))
         crosslisted.save()
         for course_id in crosslistedCourses:
-            course_prefix=BannerCourses.get(BannerCourses.reFID == int(course_id)).subject_id
+            course_prefix = BannerCourses.get(
+                BannerCourses.reFID == int(course_id)).subject_id
             cc_course = Course(bannerRef=course_id,
-                    prefix = course_prefix,
-                    term = int(tid),
-                    schedule = values['schedule'],
-                    capacity = values['capacity'],
-                    specialTopicName = values['specialTopicName'],
-                    notes = values['requests'],
-                    crossListed = True,
-                    parentCourse = course.cId,
-                    section = values['section'],
-                    prereq = convertPrereqs(prereqs)
-                    )
+                               prefix=course_prefix,
+                               term=int(tid),
+                               schedule=values['schedule'],
+                               capacity=values['capacity'],
+                               specialTopicName=values['specialTopicName'],
+                               notes=values['requests'],
+                               crossListed=True,
+                               parentCourse=course.cId,
+                               section=values['section'],
+                               prereq=convertPrereqs(prereqs)
+                               )
             cc_course.save()
             databaseInterface.addCourseInstructors(instructors, cc_course.cId)
             crosslisted = CrossListed(
-                        courseId=course.cId,
-                        crosslistedCourse=cc_course.cId,
-                        prefix=course_prefix,
-                        verified = False,
-                        term=int(tid)
-                    )
-                    
+                courseId=course.cId,
+                crosslistedCourse=cc_course.cId,
+                prefix=course_prefix,
+                verified=False,
+                term=int(tid)
+            )
+
             crosslisted.save()
-   
-    
-    
+
 
 @app.route("/addOne/<tid>", methods=["POST"])
 def add_one(tid):
     data = request.form
-    course= Course.get(Course.cId==data["courses"]) #get an existing course
+    # get an existing course
+    course = Course.get(Course.cId == data["courses"])
 
-    #create a new course using fields from an existing course because we are importing it as new
+    # create a new course using fields from an existing course because we are
+    # importing it as new
     course = Course(bannerRef=course.bannerRef_id,
                     prefix=course.prefix_id,
                     term=int(tid),
@@ -174,15 +191,16 @@ def add_one(tid):
                     )
     course.save()
 
-     #if there are instructors for an existing course, update instructors of new course as well
-    for instructor in InstructorCourse.select().where(InstructorCourse.course_id==data["courses"]):
+    # if there are instructors for an existing course, update instructors of
+    # new course as well
+    for instructor in InstructorCourse.select().where(
+            InstructorCourse.course_id == data["courses"]):
         if instructor:
-            course_instructor=InstructorCourse(
-                username_id = instructor.username_id,
-                course_id = course.cId
-                )
+            course_instructor = InstructorCourse(
+                username_id=instructor.username_id,
+                course_id=course.cId
+            )
             course_instructor.save()
-
 
     return redirect(redirect_url())
 
@@ -193,32 +211,34 @@ def add_many(tid):
     courses = request.form.getlist('courses')
     if courses:
         for i in courses:
-            course=Course.get(Course.cId==int(i)) #get an existing course
-            #create a new course using fields from an existing course because we are importing it as new
+            course = Course.get(Course.cId == int(i))  # get an existing course
+            # create a new course using fields from an existing course because
+            # we are importing it as new
             course = Course(
-                        bannerRef=course.bannerRef_id,
-                        prefix=course.prefix_id,
-                        term=int(tid),
-                        schedule=course.schedule_id,
-                        capacity=course.capacity,
-                        specialTopicName=course.specialTopicName,
-                        notes=None,
-                        crossListed=int(course.crossListed),
-                        rid=None,
-                        section = course.section,
-                        prereq=course.prereq
-                        )
-            
+                bannerRef=course.bannerRef_id,
+                prefix=course.prefix_id,
+                term=int(tid),
+                schedule=course.schedule_id,
+                capacity=course.capacity,
+                specialTopicName=course.specialTopicName,
+                notes=None,
+                crossListed=int(course.crossListed),
+                rid=None,
+                section=course.section,
+                prereq=course.prereq
+            )
+
             course.save()
 
-
-     #if there are many instructors for an existing course, update instructors of new course as well
-            for instructor in InstructorCourse.select().where(InstructorCourse.course_id==int(i)):
+     # if there are many instructors for an existing course, update instructors
+     # of new course as well
+            for instructor in InstructorCourse.select().where(
+                    InstructorCourse.course_id == int(i)):
                 if instructor:
-                    course_instructor=InstructorCourse(
-                        username_id = instructor.username_id,
-                        course_id = course.cId
-                        )
+                    course_instructor = InstructorCourse(
+                        username_id=instructor.username_id,
+                        course_id=course.cId
+                    )
                     course_instructor.save()
 
     return redirect(redirect_url())
@@ -227,22 +247,26 @@ def add_many(tid):
 @app.route('/get_termcourses/<term>/<department>')
 def term_courses(term, department):
     '''returns all courses for a specific term to ajax call when importing one/many course from terms'''
-    
+
     try:
-        term1=Term.get(Term.name == term)
-        courses_dict={}
-     
-        courses = Course.select().where(Course.prefix == department, Course.term == term1.termCode)
+        term1 = Term.get(Term.name == term)
+        courses_dict = {}
+
+        courses = Course.select().where(
+            Course.prefix == department,
+            Course.term == term1.termCode)
         if courses:
-            for course in courses :
+            for course in courses:
                 bannerNumber = str(course.bannerRef.number)[-2:]
                 # Don't add x86 courses
                 if bannerNumber != '86':
-                    courses_dict[course.cId]= model_to_dict(course)
+                    courses_dict[course.cId] = model_to_dict(course)
                     if course.schedule:
                         if course.schedule != "ZZZ":
-                            courses_dict[course.cId]["schedule"]["startTime"]= str(courses_dict[course.cId]["schedule"]["startTime"].strftime("%I:%M %p"))
-                            courses_dict[course.cId]["schedule"]["endTime"]= str(courses_dict[course.cId]["schedule"]["endTime"].strftime("%I:%M %p"))
+                            courses_dict[course.cId]["schedule"]["startTime"] = str(
+                                courses_dict[course.cId]["schedule"]["startTime"].strftime("%I:%M %p"))
+                            courses_dict[course.cId]["schedule"]["endTime"] = str(
+                                courses_dict[course.cId]["schedule"]["endTime"].strftime("%I:%M %p"))
                             courses_dict[course.cId]["schedule_object"] = True
                         else:
                             courses_dict[course.cId]["schedule_object"] = False
@@ -253,14 +277,14 @@ def term_courses(term, department):
                     inst = InstructorCourse.select().where(InstructorCourse.course == course)
                     courses_dict[course.cId]["instructors"] = []
                     for instructor in inst:
-                        courses_dict[course.cId]["instructors"].append(instructor.username.firstName[0]+ ". " + instructor.username.lastName)
-		else:
+                        courses_dict[course.cId]["instructors"].append(
+                            instructor.username.firstName[0] + ". " + instructor.username.lastName)
+                else:
                     pass
-        print("Sending courses to JS") 
+        print("Sending courses to JS")
         return json.dumps(courses_dict)
-    except:
+    except BaseException:
         return json.dumps("Error")
-
 
 
 @app.route("/courses/get_sections/", methods=["POST"])
@@ -271,17 +295,23 @@ def get_sections():
     try:
         term = int(term)
     except ValueError:
-	return jsonify(list())
+        return jsonify(list())
     edit = False
     if "edit" in request.json:
         edit = True
     prefix, number, section = course.split(" ", 2)
-    bannerCourse = BannerCourses.select().where(BannerCourses.subject == prefix).where(BannerCourses.number == number)
+    bannerCourse = BannerCourses.select().where(
+        BannerCourses.subject == prefix).where(
+        BannerCourses.number == number)
     if bannerCourse.exists():
         bRef = bannerCourse.get().reFID
-        current_courses = Course.select().where(Course.bannerRef == bRef).where(Course.term == term)
+        current_courses = Course.select().where(
+            Course.bannerRef == bRef).where(
+            Course.term == term)
         if "86" in number:
-            current_courses = SpecialTopicCourse.select().where(SpecialTopicCourse.bannerRef == bRef).where(SpecialTopicCourse.term == term)
+            current_courses = SpecialTopicCourse.select().where(
+                SpecialTopicCourse.bannerRef == bRef).where(
+                SpecialTopicCourse.term == term)
         existing_section = list()
         sections = list()
         if edit and section is not None:
@@ -301,18 +331,14 @@ def get_sections():
 def generate_sections(existing_section):
     sections = list()
     for i in range(1, 3):
-        for letter in range(65,91):
+        for letter in range(65, 91):
             letter = chr(letter) * i
             if letter not in existing_section:
                 sections.append(letter)
     return sections
 
+
 @app.route("/test_form", methods=["POST"])
 def form_sample():
     data = request.form
     return "The parameter was: {0}".format(data['var1'])
-
-
-
-
-    
