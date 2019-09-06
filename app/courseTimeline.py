fc2c42bcc97e0bb6ab0e931fac75ed8e74547ea7
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-09-06 12:20:39 -0400

    Got rid of comment

--- app/courseTimeline.py
+++ app/courseTimeline.py
@@ -12,7 +12,7 @@ from flask import json, jsonify
 def courseTimeline(tid):
   #This information has to be passed for the courseManagementSidebar...
   #TODO: Turn this information into a json call so that it doesn't have to be
-  #passed in every course management controller. 
+  #passed in every course management controller.
   terms = Term.select().order_by(-Term.termCode)
   if tid == 0:
     tid = terms[0].termCode
@@ -22,26 +22,26 @@ def courseTimeline(tid):
                             page=page,
                             currentTerm=int(tid),
                             cfg = cfg)
-                            
+
 @app.route('/courseTimeline/<tid>/json', methods=["GET","POST"])
 def timelineJson(tid):
   if int(tid) == 0:
     terms = Term.select().order_by(-Term.termCode)
     tid   = terms[0].termCode
-  print (type(tid))
+  # print (type(tid))
   google_chart_dict = dict()
   timeline_obj = timeline()
   for key, day in cfg['scheduleDaysShort'].items():
     schedule_info = dict()
     schedule_list  = []
-    schedules = getCourseTimelineSchedules(day,tid)    
+    schedules = getCourseTimelineSchedules(day,tid)
     for schedule in schedules:
       try:
         num_of_courses = Course.select().where(Course.schedule == schedule.schedule).where(Course.term == tid).count()
       except Exception as e:
         num_of_courses = 0
       schedule_info[schedule.schedule.sid] = [num_of_courses, schedule.schedule.startTime, schedule.schedule.endTime]
-      schedule_list.append(schedule.schedule.sid)  
+      schedule_list.append(schedule.schedule.sid)
     timeline_obj.collect_schedule_details(schedule_info,schedule_list)
     google_chart_data = timeline_obj.google_chart_data()
     google_chart_dict[key]=google_chart_data
@@ -50,5 +50,3 @@ def timelineJson(tid):
   except Exception as e:
     print str(e)
   return jsonify(json_str)
- 
-  
