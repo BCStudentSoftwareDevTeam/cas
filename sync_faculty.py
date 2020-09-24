aaba178bdfbd176ed7d960f371a14a8f7a0f3a3c
Scott Heggen <heggens@berea.edu>
2020-09-24 14:13:54 -0400

    fixing sync_faculty script for py3

--- sync_faculty.py
+++ sync_faculty.py
@@ -1,7 +1,7 @@
 from ldap3 import Server, Connection, ALL
 from peewee import *
 from os import path, remove
-from app.models import *
+from app.models.models import *
 import argparse
 
 # Create database connection and model
@@ -98,8 +98,8 @@ def add_members():
             if user.firstname is None or user.lastname is None:
                 print(user.username + " has null fields skipping")
                 continue
-	    if user.username == "Colesa":		# Sam Cole has two records, one with upper case. Skip hers.
-		continue
+            if user.username == "Colesa":		# Sam Cole has two records, one with upper case. Skip hers.
+                continue
             new_user = User.create(
                     username = user.username,
                     bNumber = user.bnumber,
