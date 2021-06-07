f7c4ba764fc853bcfae2f88965a264ac1ba7c748
None <partont@ssdt2021partont.ssdt.berea.edu>
2021-06-07 16:06:39 -0400

    Removed error handling and html page for error 401.

--- app/controllers/error_routes/errorHandler.py
+++ app/controllers/error_routes/errorHandler.py
@@ -4,10 +4,6 @@ from app import *
 
 from app.allImports import *
 
-@app.errorhandler(401)
-def unauthorized(e):
-    return render_template('/snips/errors/401.html', cfg = cfg), 401
-
 @app.errorhandler(403)
 def unauthenticated(e):
     return render_template('/snips/errors/403.html', cfg = cfg), 403
