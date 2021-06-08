5ea3dd50514101c742e0cae70ba7e45cc7a8159a
None <lamichhanes2@ssdt2021lamichhanes2.ssdt.berea.edu>
2021-06-08 13:23:51 -0400

    linked popup.js to popover in adminCourseTable

--- app/static/js/local/popup.js
+++ app/static/js/local/popup.js
@@ -1,7 +1,12 @@
 // on click make a pop-out for the notes
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover({
-        placement : 'top',
-        trigger : 'focus',
-    });
+        placement : 'bottom',
+        animation : false,
+        html : true
+      });
+        
+      $(document).on("click", ".popover .close" , function(){
+          $(this).parents(".popover").popover('hide');
+      });
 });
