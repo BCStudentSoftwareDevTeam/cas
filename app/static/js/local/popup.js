03689e463d51bd73c0c146a1327da7ddf55e7968
escalerapadronl <escalerapadronl@berea.edu>
2021-06-08 17:09:33 -0400

    fixed notes overlapping and added close icon to notes modal. changed info popover on editActiveCourses to tooltip

--- app/static/js/local/popup.js
+++ app/static/js/local/popup.js
@@ -1,12 +1,17 @@
 // on click make a pop-out for the notes
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover({
-        placement : 'bottom',
-        animation : false,
-        html : true
+        placement : 'top',
+        html : true,
+        trigger : 'focus',
+        title : 'Course Notes <a href="#" class="close" data-dismiss="alert">&times;</a>'
       });
-        
-      $(document).on("click", ".popover .close" , function(){
-          $(this).parents(".popover").popover('hide');
-      });
+
+    $(document).on("click", ".popover .close" , function(){
+        $('[data-toggle="popover"]').parents(".popover").popover('hide');
+    });
+
+    $('[data-toggle="tooltip"]').tooltip({
+      placement : 'top'
+    })
 });
