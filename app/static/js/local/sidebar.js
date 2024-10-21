14757ace556a708beaaba4a06ed05993558030f9
hoerstl <hoerstl@berea.edu>
2024-10-21 18:00:14 +0000

    Started modifying the sidebar

--- app/static/js/local/sidebar.js
+++ app/static/js/local/sidebar.js
@@ -1,7 +1,15 @@
 /* global $ */
 
-$('.first-level').on('click', function(){
-   $(this).children().toggleClass('glyphicon-chevron-down');
-   $(this).children().toggleClass('glyphicon-chevron-up');
+$(document).ready(function(){
+   $('.first-level').on('click', function(){
+      $(this).children().toggleClass('glyphicon-chevron-down');
+      $(this).children().toggleClass('glyphicon-chevron-up');
+   
+   })
+   
 
+   $('#divisionSelect').on('change', function(){
+      console.log("division select changed")
+   })
 })
+
